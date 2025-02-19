c79b7ab51fb9f1728caa2d4b8e947dfca0301afb
haianh3623 <anhldh.dev@gmail.com>
2025-02-19 09:55:31 +0700

    init project

---
+++ src/main/java/group/assignment/booking_hotel_backend/BookingHotelBackendApplication.java
@@ -0,0 +1,13 @@
+package group.assignment.booking_hotel_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingHotelBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingHotelBackendApplication.class, args);
+	}
+
+}
