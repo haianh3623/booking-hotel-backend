397103ffd28e920c72b6f6cee3bccd21a205eb3d
pth573 <phamhang572003@gmail.com>
2025-04-06 01:38:07 +0700

    update base entity

---
+++ src/main/java/group/assignment/booking_hotel_backend/config/AuditAwareImpl.java
@@ -0,0 +1,14 @@
+package group.assignment.booking_hotel_backend.config;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component("auditAwareImpl")
+public class AuditAwareImpl implements AuditorAware<String> {
+    @Override
+    public Optional<String> getCurrentAuditor() {
+        return Optional.of("HOTEL_USER");
+    }
+
+}
